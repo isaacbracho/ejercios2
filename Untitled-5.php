b44589344b7f059b505668e0e39905d38f26b56d
isaacbracho <isaacbracho676@gmail.com>
2025-08-04 11:06:31 -0400

    Update Untitled-5.php

---
+++ Untitled-5.php
@@ -0,0 +1,14 @@
+<?php
+
+$numero = 0; 
+
+echo "se esta generando los numeros..\n";
+
+while ($numero <= 50) {
+    $numero = rand(1, 100); 
+    echo "nemero exitoso: " . $numero . "\n";
+}
+
+echo "¡se genero el mayor 50.";
+
+?>
